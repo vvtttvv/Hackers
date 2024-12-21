dc8010e7eb220da0fbf74db3abfc70452012e227
Мдфв <v.v.titerez@gmail.com>
2024-12-21 18:02:02 +0200

    Added statistics

--- front/my-react-app/src/features/Left/left.jsx
+++ front/my-react-app/src/features/Left/left.jsx
@@ -5,7 +5,16 @@ export default function Left() {
 
   return (
     <div className={styles.wrapper}>
-        dsgsg
+        <div className={styles.statistic}>
+          <span class="material-symbols-outlined">home</span>
+          <p>5k+</p>
+          <div className={styles.line}></div>
+          <p> $15000 </p>
+        </div>
+
+        <div className={styles.messages}>
+          Here are messages
+        </div>
     </div>
   )
 }
