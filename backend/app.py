ceefc00b2665c9d5f5d12d88476f8e07640aaf36
Andre Chicu <andriesh.chicu@gmail.com>
2024-12-21 22:35:41 +0200

    h

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,106 @@ from flask import jsonify
 from flask import request
 from flask_cors import CORS
 
+import os
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import tempfile
+
+
+load_dotenv()
+groq_api_key = os.getenv('GROQ_API_KEY')
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
 app = Flask(__name__)
 
 CORS(app)
 
 dummy_invoice_data = [
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "email": "test@mail.ru",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    },
     {
         "issued_by": "Lorem Ipsum",
         "issued_on": "ddmmyyyy",
@@ -101,7 +196,9 @@ Here are your steps:\n
 1. step 1\n
 2. one more step\n
 3. last step :3\n
-""" + q
+""" + q + """\n
+[Form](https://www.asp.gov.md/sites/default/files/servicii/e-servicii/formulare-tip/2/inregistrarea-de-stat/16-Cerere-inregistrare-intreprinzatorilor-individuali.pdf)
+"""
 
 
 if __name__ == "__main__":
