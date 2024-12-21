69cd3e468165ce8dcc4ab02f55bae81857336eca
Andre Chicu <andriesh.chicu@gmail.com>
2024-12-21 18:41:51 +0200

    dummy data???

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,21 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+dummy_data = [
+    {
+        "issued_by": "Lorem Ipsum",
+        "issued_on": "ddmmyyyy",
+        "due_date": "ddmmyyyy",
+        "product": "test product",
+        "quantity": 69,
+        "unit_price": 100,
+        "subtotal": 88,
+        "tva": 33,
+        "total": 76,
+        "bank_name": "Xd",
+    }
+]
+
 
 @app.route("/")
 def home():
@@ -11,11 +26,7 @@ def home():
 
 @app.route("/api/header/")
 def header():
-    data = {
-        "1": "text1",
-        "2": "text2"
-    }
-    return jsonify(data)
+    return jsonify(dummy_data)
 
 
 if __name__ == "__main__":
